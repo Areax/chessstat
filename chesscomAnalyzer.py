92104d4f63f669bf98587da74e80d67561702ba3
areax <actuallythisisspam@gmail.com>
2018-02-13 04:10:13 -0500

    Updates chessStats

--- chesscomAnalyzer.py
+++ chesscomAnalyzer.py
@@ -3,6 +3,7 @@
 import requests 
 import time
 import json
+from pathlib import Path
 
 pre_analyzed = []
 
@@ -14,12 +15,26 @@ online_pgns = open('allGames.pgn', 'w')
 games = {}
 games["games"] = []
 
+openings = json.loads(Path('dictOfOpenings.txt').read_text())
+opening_keys = sorted(list(openings.keys()), key=len)
+opening_keys.reverse()
+
+
 for page in [1,2,3]:
 	temp = requests.get("https://api.chess.com/pub/player/" + USERNAME + "/games/2017/" + str(11 - page + 1)).json()
 	if("games" in temp.keys()):
 		games["games"] = games["games"] + temp["games"]
 	time.sleep(2)
 
+	 
+	for game in games["games"]:
+		for key in opening_keys:
+			try:
+				if(key in game["pgn"]):
+					game["Opening"] = openings[key]
+			except Exception as e:
+				game["Opening"] = "unknown"
+	
 online_pgns.write(json.dumps(games, indent=4, sort_keys=True))
 #for game in games["currentPageResults"]: 
 
